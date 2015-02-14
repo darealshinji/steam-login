a8ee00b4750d7fbe35e4052e1f1b4943eeb680e3
darealshinji <djcj@gmx.de>
2015-02-14 03:23:55 +0100

    force deletion of debian-tmp

--- build-debian-package.sh
+++ build-debian-package.sh
@@ -2,11 +2,11 @@
 
 cd `dirname $0`
 
-rm -r debian-tmp && mkdir debian-tmp
+rm -rf debian-tmp && mkdir debian-tmp
 cp -r packaging/debian steam-session/* debian-tmp
 
 cd debian-tmp
 fakeroot dpkg-buildpackage -b -us -uc
 cd ..
-rm -r debian-tmp
+rm -rf debian-tmp
 
